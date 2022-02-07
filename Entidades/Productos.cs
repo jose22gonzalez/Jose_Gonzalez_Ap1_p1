0115b6cc46617aac387599f2a69215ce63b10677
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-02-07 19:37:14 -0400

    Creando migracion y la base de dato

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Jose_Gonzalez_Ap1_p1.Entidades
 {
     public class Productos
     {
-        public int Productoid { get; set; }
+        [Key] public int Productoid { get; set; }
         public string? Descripcion { get; set; }
         public string? Existencia { get; set; }
         public int Costo { get; set; }
---
+++ Migrations/20220207233248_InicialMigracion.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jose_Gonzalez_Ap1_p1.Migrations
+{
+    public partial class InicialMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Productoid = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Existencia = table.Column<string>(type: "TEXT", nullable: true),
+                    Costo = table.Column<int>(type: "INTEGER", nullable: false),
+                    ValorInventario = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Productoid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
