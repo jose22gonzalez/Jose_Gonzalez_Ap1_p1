9c76b798853a7fc90812e630e29507f2574360a3
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-02-07 18:45:06 -0400

    Creando Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Jose_Gonzalez_Ap1_p1.Entidades;
+
 public class Contexto:DbContext
 {
+    public DbSet<Productos> Productos { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //optionsBuilder.UseSqlite(@"Data Source=Data\.db");
+        optionsBuilder.UseSqlite(@"Data Source=Data\Productos.db");
     }
 }
